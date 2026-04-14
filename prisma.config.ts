6fdd28f9684f4ebbb167de2122b1fa32277770f4
Dparamet <dparamet1@gmail.com>
2026-04-14 22:15:00 +0700

    fix:prisma bug and  Error and password env

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from '@prisma/config';
+import { defineConfig } from '@prisma/config' // 1. ตรวจสอบชื่อแพ็กเกจ
+import 'dotenv/config'
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL, // 2. ต้องอยู่ใน datasource และ u ต้องเป็นตัวพิมพ์เล็ก
   },
-});
+})
